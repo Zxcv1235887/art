9eded50359e128c6029ca1e441c08ee48d6b4de1
Zxcv1235887 <belly05329@gmail.com>
2026-06-14 23:30:14 +0800

    add db

--- gallery_backend/settings.py
+++ gallery_backend/settings.py
@@ -85,8 +85,12 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '3306'),
+        'OPTIONS': {
+            'connect_timeout': 30,
+        },
     }
 }
+
 
 
 # Password validation
